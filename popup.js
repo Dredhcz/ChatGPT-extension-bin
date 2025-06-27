545c12dde1d7e0f5dfb8e2a4c250f30095af8491
Dredh <dredh@email.cz>
2025-06-28 00:10:38 +0200

    First commit

---
+++ popup.js
@@ -0,0 +1,39 @@
+document.getElementById("save").addEventListener("click", async () => {
+  const textarea = document.getElementById("message");
+  const text = textarea.value.trim();
+
+  if(text){
+    let { buffer } = await chrome.storage.local.get({ buffer: [] });
+    buffer.push(text);
+    await chrome.storage.local.set({buffer});
+    textarea.value = "";
+
+    alert(buffer);
+    refreshMessageList();
+    
+  }
+});
+async function refreshMessageList(){
+  const { buffer } = await chrome.storage.local.get({ buffer: [] });
+  const list = document.getElementById("message-list");
+
+  list.innerHTML = "";
+
+  buffer.forEach((msg, index) => {
+    const li = document.createElement("li");
+    li.textContent = msg;
+    list.appendChild(li);
+  })
+}
+async function deleteFirstElement(){
+  let { buffer } = await chrome.storage.local.get({ buffer: [] });
+  buffer.shift();
+  await chrome.storage.local.set({buffer});
+  refreshMessageList();
+}
+
+document.getElementById("delete").addEventListener("click", async () => {
+  buffer = [];
+  await chrome.storage.local.set({buffer});
+  refreshMessageList();
+});
